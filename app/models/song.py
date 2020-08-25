e65dd1b81ad9de3487053c192148466b80c75858
Tina <me@tina.moe>
2020-08-25 16:34:33 +0200

    Change name_localized Person column to name_romanized

--- app/models/song.py
+++ app/models/song.py
@@ -23,7 +23,7 @@ class Artist(db.Model):
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    name_localized = db.Column(db.String)
+    name_romanized = db.Column(db.String)
     birthday = db.Column(db.DateTime)
     description = db.Column(db.String)
 
