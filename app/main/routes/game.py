bbfb000dc7393ba74fd0908c37636817d7a1dbc9
Tina <me@tina.moe>
2021-04-16 23:54:45 +0200

    Add game version route

--- app/main/routes/game.py
+++ app/main/routes/game.py
@@ -1,14 +1,20 @@
 from .. import bp
-from ..models import Game
+from ..models import Game, Version
 
 @bp.route('/games')
 def games():
     return {
-        x.key: x.dictify()
+        x.key: x
         for x in Game.query.all()
     }
 
 @bp.route('/games/<string:key>')
 def game(key):
     result = Game.query.filter_by(key=key).first_or_404()
+    return result.dictify()
+
+@bp.route('/games/<string:game>/<string:version>')
+def game_version(game, version):
+    game = Game.query.filter_by(key=game).first_or_404()
+    result = Version.query.filter_by(key=version, game=game).first_or_404()
     return result.dictify()
